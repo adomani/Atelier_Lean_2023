190ff0948502c1bbc5584f5986e472c64e56fb38
adomani <adomani@gmail.com>
2023-04-18 08:56:27 +0200

    Remove one space

--- autolinks.sh
+++ autolinks.sh
@@ -13,7 +13,7 @@ getLinkRef () {
 }
 
 if [ "$(whoami)" == "damiano" ]; then
-  .  ~/Setup/Tests/testtest.sh
+  . ~/Setup/Tests/testtest.sh
   outerret 'printf "text [1][2], more text, [3][4]\n[5][6]" | getLinkRef -' $'2\n4\n6\n' '' 0
 fi
 
